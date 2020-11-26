3d38c1338a6c3b5f09f7006e71b40a2e2b10729b
Peter Cao <peterspellingbee@gmail.com>
2020-11-26 05:33:49 +0000

    Made infinite scraper

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import scraper
+if __name__ == "__main__":
+	while True:
+		query = input("What do you want to search for? ")
+		if query.lower() == "quit":
+			break
+		print()
+		scraper.endlessScrapeFromSearch(query)
+		print()
 
-
-print(scraper.getUrlFromSearch("Center High School"))
--- scraper.py
+++ scraper.py
@@ -4,11 +4,44 @@ import random
 import wikipedia
 import html5lib
 import warnings
+import time
 
 warnings.filterwarnings('ignore')
 
+def endlessScrapeFromSearch(searchQuery):
+	scrapeSite(getUrlFromSearch(searchQuery))
 
+def scrapeSite(url):
+	try:
+		response = requests.get(url = url)
+	except:
+		return
+	soup = BeautifulSoup(response.content,
+		'html.parser')
+	
+	title = soup.find(id="firstHeading")
+	print(title.text)
 
+	allLinks = soup.find(id="bodyContent").find_all("a")
+	random.shuffle(allLinks)
+	linkToScrape = 0
+	for link in allLinks:
+		# We are only interested in other wiki articles
+		try:
+			if link['href'].find("/wiki/") == -1: 
+				continue
+		except KeyError:
+			return
+		# Use this link to scrape
+		linkToScrape = link
+		break
+	
+	time.sleep(1)
+	try:
+		scrapeSite("https://en.wikipedia.org" + linkToScrape['href'])
+	except TypeError: 
+		return
+		
 def search(search):
 	return wikipedia.search(search)
 
