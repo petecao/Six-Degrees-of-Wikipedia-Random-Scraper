e4a0541f0c23ff5bc9fbfd4124449973af3c550c
Peter Cao <peterspellingbee@gmail.com>
2020-11-26 00:27:49 +0000

    Added function to get URL from search

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import scraper
-import warnings
-import wikipedia
 
-warnings.filterwarnings('ignore')
-try:
-	scraper.getUrl((scraper.search("Center High School"))[0])
-except wikipedia.exceptions.DisambiguationError:
-	print(scraper.getUrl((scraper.search("Center High School"))[1]))
+
+print(scraper.getUrlFromSearch("Center High School"))
--- scraper.py
+++ scraper.py
@@ -3,14 +3,23 @@ from bs4 import BeautifulSoup
 import random
 import wikipedia
 import html5lib
+import warnings
 
+warnings.filterwarnings('ignore')
 
 
 
 def search(search):
-	print(wikipedia.search(search))
 	return wikipedia.search(search)
 
 def getUrl(page):
 	soup = BeautifulSoup(wikipedia.page(page).url, features="html.parser")
 	return soup
+
+def getUrlFromSearch(searchQuery):
+	searchResults = search(searchQuery)
+	for result in searchResults:
+		try:
+			return getUrl(result)
+		except:
+			pass
